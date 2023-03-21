9bde9dc4218750b1adfe3b0dfb1aa4afb97a2e8f
Guilherme Castilhone <gfcastilhone@gmail.com>
2023-03-21 15:04:07 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,24 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate data = LocalDate.parse("23/09/1998", dtf);
+		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "Bob@gmail.com"
+				,  data, 3000.00, obj);
+		
+		
+		System.out.println(seller);
 	}
 }
